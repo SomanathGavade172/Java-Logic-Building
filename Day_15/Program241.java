83cf197f578a098f9d3bdb5b0bfb484e3f954380
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-05-07 22:37:46 +0530

    Day 15: Added Java Advanced Programs

---
+++ Day_15/Program241.java
@@ -0,0 +1,69 @@
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name      : Digit
+//  Description     : Contains function to calculate multiplication of digits.
+//  Input           : Integer
+//  Output          : Returns multiplication of digits
+//  Author          : Gavade Somanath Vilas
+//  Date            : 07/05/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Digit
+{
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   : MultiplicationDigits
+//  Description     : Calculates multiplication of digits of given number.
+//  Input           : Integer
+//  Output          : Integer
+//  Time Complexity : O(N)
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public int MultiplicationDigits(int iNo)
+    {
+        int iDigit = 0, iMulti = 1;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+
+            iMulti = iMulti * iDigit;
+
+            iNo = iNo / 10;
+        }
+
+        return iMulti;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name      : Program241
+//  Description     : This application accepts number from user and displays multiplication of digits.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Program241
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0, iRet = 0;
+
+        System.out.println("Enter a Number : ");
+        iValue = sobj.nextInt();
+
+        Digit dobj = new Digit();
+        
+        iRet = dobj.MultiplicationDigits(iValue);
+
+        System.out.println("Multiplication of digits : " + iRet);
+
+    }
+}
