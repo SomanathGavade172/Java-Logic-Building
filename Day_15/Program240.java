5fd93901af5c9b01aaea99a57b92a880d1472c63
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-05-07 22:36:55 +0530

    Day 15: Added Java Advanced Programs

---
+++ Day_15/Program240.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name      : Numbers
+//  Description     : Contains function to calculate addition of even factors.
+//  Input           : Integer
+//  Output          : Returns addition of even factors
+//  Author          : Gavade Somanath Vilas
+//  Date            : 07/05/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Numbers
+{
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   : EvenFactors
+//  Description     : Calculates addition of even factors of given number.
+//  Input           : Integer
+//  Output          : Integer
+//  Time Complexity : O(N/2)
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public int EvenFactors(int iNo)
+    {
+        int iCnt = 0, iSum = 0;
+
+        // Filter
+        if(iNo % 2 != 0)
+        {
+            return 0;
+        }
+
+        for(iCnt = 2; iCnt <= (iNo / 2); iCnt = iCnt + 2)
+        {
+            if(iNo % iCnt == 0)
+            {
+                iSum = iSum + iCnt;
+            }
+        }
+
+        return iSum;
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name     : Program240
+//  Description    : This application accepts number from user and displays addition of even factors.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Program240
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0, iRet = 0;
+
+        System.out.println("Enter a Number : ");
+        iValue = sobj.nextInt();
+
+        Numbers nobj = new Numbers();
+
+        iRet = nobj.EvenFactors(iValue);
+
+        System.out.println("Addition of Even Factors is : " + iRet);
+    }
+}
