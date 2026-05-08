a49223d4dee4a952b49223ef490efe915cd60dc7
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-05-08 21:21:13 +0530

    Day 15: Added Java Advanced Programs

---
+++ Day_15/Program250.java
@@ -0,0 +1,123 @@
+// 883042
+// 0 : 1
+// 2 : 1
+// 3 : 1
+// 4 : 1
+// 8 : 2
+
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name      : Digits
+//  Description     : Contains function to count frequency of digits.
+//  Input            : Integer
+//  Output           : Displays frequency of digits
+//  Author           : Gavade Somanath Vilas
+//  Date             : 07/05/2026
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Digits
+{
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name    : CountDigits
+//  Description      : Counts frequency of digits in given number.
+//  Input            : Integer
+//  Output           : Displays frequency of digits
+//  Time Complexity  : O(N)
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public void CountDigits(int iNo)
+    {
+        int iCount[] = {0,0,0,0,0,0,0,0,0,0};
+
+        int iDigit = 0;
+
+        while(iNo != 0)
+        {
+            iDigit = iNo % 10;
+
+            if(iDigit == 0)
+            {
+                iCount[0]++;
+            }
+            else if(iDigit == 1)
+            {
+                iCount[1]++;
+            }
+            else if(iDigit == 2)
+            {
+                iCount[2]++;
+            }
+            else if(iDigit == 3)
+            {
+                iCount[3]++;
+            }
+            else if(iDigit == 4)
+            {
+                iCount[4]++;
+            }
+            else if(iDigit == 5)
+            {
+                iCount[5]++;
+            }
+            else if(iDigit == 6)
+            {
+                iCount[6]++;
+            }
+            else if(iDigit == 7)
+            {
+                iCount[7]++;
+            }
+            else if(iDigit == 8)
+            {
+                iCount[8]++;
+            }
+            else if(iDigit == 9)
+            {
+                iCount[9]++;
+            }
+
+            iNo = iNo / 10;
+        }
+
+        System.out.println("Frequency of 0 is : " + iCount[0]);
+        System.out.println("Frequency of 1 is : " + iCount[1]);
+        System.out.println("Frequency of 2 is : " + iCount[2]);
+        System.out.println("Frequency of 3 is : " + iCount[3]);
+        System.out.println("Frequency of 4 is : " + iCount[4]);
+        System.out.println("Frequency of 5 is : " + iCount[5]);
+        System.out.println("Frequency of 6 is : " + iCount[6]);
+        System.out.println("Frequency of 7 is : " + iCount[7]);
+        System.out.println("Frequency of 8 is : " + iCount[8]);
+        System.out.println("Frequency of 9 is : " + iCount[9]);
+    }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name      : Program250
+//  Description     : This application accepts number from user and displays frequency of digits.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Program250
+{
+    public static void main(String Arg[])
+    {
+        Scanner sobj = new Scanner(System.in);
+
+        int iValue = 0;
+
+        System.out.println("Enter a number : ");
+        iValue = sobj.nextInt();
+
+        Digits dobj = new Digits();
+
+        dobj.CountDigits(iValue);
+    }
+}
