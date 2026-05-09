1cd795226123233159cede8b257f9358408aa90f
SomanathGavade172 <somanathgavade2001@gmail.com>
2026-05-09 22:49:28 +0530

    Day 15: Added Java Advanced Programs

---
+++ Day_15/Program254.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Class Name      : Program254
+//  Description     : This application demonstrates array initialization and traversal.
+//  Input            : Integer Array
+//  Output           : Displays elements of array and size of array
+//  Author           : Gavade Somanath Vilas
+//  Date             : 08/05/2026
+//  Time Complexity  : O(N)
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Program254
+{
+    public static void main(String Arg[])
+    {
+        int Arr[] = {10, 20, 30, 40, 50};
+
+        System.out.println(Arr[0]);
+        System.out.println(Arr[1]);
+        System.out.println(Arr[2]);
+        System.out.println(Arr[3]);
+        System.out.println(Arr[4]);
+
+        System.out.println("Size of Array is : " + Arr.length);
+    }
+}
